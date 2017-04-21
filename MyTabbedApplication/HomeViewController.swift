886fbe2865567492d0a1d81f1cf8924b1b5f7b0b
kesen <superkesen@gmail.com>
2017-04-21 17:36:53 +0800

    添加第三方库

--- MyTabbedApplication/HomeViewController.swift
+++ MyTabbedApplication/HomeViewController.swift
@@ -13,11 +13,18 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Nav 左边按钮
+        let leftBtn = UIButton(type: .custom)
+        leftBtn.backgroundColor = UIColor.clear
+        leftBtn.frame = CGRect(x: 0, y: 0, width: 70, height: 70)
+        leftBtn.imageEdgeInsets = UIEdgeInsetsMake(0, -40, 0, 0)
+        leftBtn.setImage(UIImage(named: "Mine"), for: .normal)
+        leftBtn.setImage(UIImage(named: "mineSelected"), for: .highlighted)
+        leftBtn.addTarget(self, action: #selector(self.leftBarButtomItemClicked(sender:)), for: .touchUpInside)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftBtn)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func leftBarButtomItemClicked(sender: UIButton) {
+        print(#function)
     }
 }
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, ‘8.0’
+
+target 'MyTabbedApplication' do
+    use_frameworks!
+
+    pod 'PKRevealController'
+    pod 'Alamofire', '~> 4.4'
+    pod 'AlamofireNetworkActivityIndicator', '~> 2.0'
+    
+    pod 'SwiftyJSON'
+    pod 'ObjectMapper', '~> 1.3'
+    pod 'SnapKit'
+    pod 'DeviceKit', '~> 1.0'
+    pod 'ReactiveCocoa', '~> 5.0.0'
+    pod 'RxSwift'
+
+end
